48cbb1e040f1168190ffe612778bb151c69e7a53
Megan Bohl <meganbohl@outlook.com>
2018-01-07 02:08:28 +0000

    initial commit

---
+++ cookbooks/feature_website/README.md
@@ -0,0 +1,4 @@
+# feature_website
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/feature_website/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: feature_website
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
